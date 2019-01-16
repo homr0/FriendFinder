62d8275ab560b78ac9ed17f716a2bf8e5d32c6a3
Rebecca Hom <rebecca.hom001@gmail.com>
2019-01-15 17:30:22 -0800

    added some dummy data

---
+++ app/data/friends.js
@@ -0,0 +1,35 @@
+[
+    {
+        "name": "Ahmed",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+
+    {
+        "name": "Amy",
+        "photo": "https://en.wikipedia.org/wiki/Amy_Poehler#/media/File:Amy_Poehler_(8894155873)_(cropped).jpg",
+        "scores": [
+            3,
+            2,
+            1,
+            4,
+            2,
+            3,
+            1,
+            5,
+            2,
+            3
+        ]
+    }
+]
