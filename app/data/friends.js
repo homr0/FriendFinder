892dee6fefd09239a42d93c190835bfc966cf135
Rebecca Hom <rebecca.hom001@gmail.com>
2019-01-15 20:24:19 -0800

    updated friends data to be consistent

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,35 +1,50 @@
 module.exports = [
     {
         "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo": "http://pixsector.com/cache/94bed8d5/av3cbfdc7ee86dab9a41d.png",
         "scores": [
-            5,
-            1,
-            4,
-            4,
-            5,
-            1,
-            2,
-            5,
-            4,
-            1
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
         ]
     },
-
     {
         "name": "Amy",
         "photo": "https://en.wikipedia.org/wiki/Amy_Poehler#/media/File:Amy_Poehler_(8894155873)_(cropped).jpg",
         "scores": [
-            3,
-            2,
-            1,
-            4,
-            2,
-            3,
-            1,
-            5,
-            2,
-            3
+            "3",
+            "2",
+            "1",
+            "4",
+            "2",
+            "3",
+            "1",
+            "5",
+            "2",
+            "3"
+        ]
+    },
+    {
+        "name": "Rebecca Hom",
+        "photo": "https://www.facebook.com/photo.php?fbid=528052653898835&l=e5d12250b1",
+        "scores": [
+            "3",
+            "4",
+            "2",
+            "1",
+            "4",
+            "1",
+            "3",
+            "5",
+            "4",
+            "5"
         ]
     }
 ]
