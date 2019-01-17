12ebe195fbaf436028fe4a9f3052f2584317570e
Rebecca Hom <rebecca.hom001@gmail.com>
2019-01-17 01:18:45 -0800

    added more people to compatible friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -17,7 +17,7 @@ module.exports = [
     },
     {
         "name": "Amy",
-        "photo": "https://en.wikipedia.org/wiki/Amy_Poehler#/media/File:Amy_Poehler_(8894155873)_(cropped).jpg",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Amy_Poehler_%288894155873%29_%28cropped%29.jpg/440px-Amy_Poehler_%288894155873%29_%28cropped%29.jpg",
         "scores": [
             "3",
             "2",
@@ -32,8 +32,8 @@ module.exports = [
         ]
     },
     {
-        "name": "Rebecca Hom",
-        "photo": "https://www.facebook.com/photo.php?fbid=528052653898835&l=e5d12250b1",
+        "name": "Rebecca",
+        "photo": "https://scontent-sjc3-1.xx.fbcdn.net/v/t31.0-8/475162_528052653898835_881251121_o.jpg?_nc_cat=103&_nc_ht=scontent-sjc3-1.xx&oh=d3841e5aeb10b4f3c37dbd24ea2af3d8&oe=5CD50F02",
         "scores": [
             "3",
             "4",
@@ -45,6 +45,86 @@ module.exports = [
             "5",
             "4",
             "5"
+        ]
+    },
+    {
+        "name": "Bob",
+        "photo": "https://vignette.wikia.nocookie.net/bobsburgerpedia/images/3/3e/Bobbelcher2.png/revision/latest?cb=20130114075133",
+        "scores": [
+            "2",
+            "3",
+            "1",
+            "1",
+            "5",
+            "2",
+            "4",
+            "3",
+            "4",
+            "2"
+        ]
+    },
+    {
+        "name": "Amadeus",
+        "photo": "https://terrigen-cdn-dev.marvel.com/content/prod/1x/5bc1a6-02_amadeuscho_post_master.jpg",
+        "scores": [
+            "4",
+            "1",
+            "4",
+            "3",
+            "3",
+            "4",
+            "5",
+            "1",
+            "4",
+            "4"
+        ]
+    },
+    {
+        "name": "Nora",
+        "photo": "https://fanfest.com/wp-content/uploads/2018/05/Nora-Allen-daughter-The-Flash.png",
+        "scores": [
+            "1",
+            "2",
+            "4",
+            "5",
+            "3",
+            "5",
+            "2",
+            "1",
+            "4",
+            "1"
+        ]
+    },
+    {
+        "name": "Tom",
+        "photo": "http://i.imgur.com/YjlUYDr.jpg",
+        "scores": [
+            "1",
+            "3",
+            "5",
+            "5",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "3"
+        ]
+    },
+    {
+        "name": "Sebastian",
+        "photo": "https://i.imgur.com/QVF5giH.jpg",
+        "scores": [
+            "5",
+            "5",
+            "5",
+            "2",
+            "4",
+            "1",
+            "3",
+            "5",
+            "3",
+            "4"
         ]
     }
 ]
